28746113670eaae71a37c6393afd87a4a6bbdb74
Yury Ershov <yuriy.ershov@gmail.com>
2020-05-24 01:46:15 +1000

    Add script template

---
+++ custom-script-template.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        {{name}} - New script
+// @namespace   Yury Ershov
+// @match       {{url}}
+// @grant       none
+// @version     1.0
+// @author      Yury Ershov
+// @description Description
+// ==/UserScript==
+// @noframes
+// @match https://developer.chrome.com/extensions/match_patterns
+// @include @exclude https://wiki.greasespot.net/Include_and_exclude_rules
+// @run-at document-start document-end document-idle
+
+(function() {
+  'use strict';
+
+})();
